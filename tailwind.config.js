f32b06bdee7dcfbfbc742d9921e97231636fefa6
MiJouHsieh <mijouhsieh@gmail.com>
2026-08-25 15:45:11 +0800

    style(theme): add color palette and shared styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#fffbf7",
+        dark: "#232323",
+        coral: "#F1888A",
+        purple: "#7C5BD1",
+        softCoral: "#F8D2CD",
+        softPurple: "#9A7BE0",
+      }
+    },
   },
   plugins: [],
 }
